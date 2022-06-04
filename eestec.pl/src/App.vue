ce6aabb12c1482e61c23a4c236c85f818290b7ea
drinaaf <aleksandra.furyk@gmail.com>
2022-06-04 17:30:48 +0200

    Update Header

--- eestec.pl/src/App.vue
+++ eestec.pl/src/App.vue
@@ -34,7 +34,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -42,6 +42,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: #acadae;
 }
+
 
 </style>
